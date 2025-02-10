c8541ff81201e02af1b88ece246f6f82a94659a9
marurun66 <marurun66@gmail.com>
2025-02-10 21:29:47 +0900

    등록반려동물수 추가

--- ui/prediction.py
+++ ui/prediction.py
@@ -104,7 +104,7 @@ def run_prediction():
             recommendation = "🌟입지추천🌟" if row["클러스터"] in [2, 3] else "⚠️신중한 선택필요⚠️"
             folium.Marker(
                 location=[row['위도'], row['경도']],
-                popup=folium.Popup(f'<div style="white-space: nowrap;"> {recommendation}<br> 클러스터: {row["클러스터"]}<br>동 이름: {row["지역명"]}<br> 병원 수: {int(row["병원수"])}<br>약국 수: {int(row["약국수"])}<br></div>', max_width=300),
+                popup=folium.Popup(f'<div style="white-space: nowrap;"> {recommendation}<br> 클러스터: {row["클러스터"]}<br>동 이름: {row["지역명"]}<br> 병원 수: {int(row["병원수"])}<br>약국 수: {int(row["약국수"])}<br>등록 반려동물수: {int(row["반려동물수"])}</div>', max_width=300),
                 icon=folium.Icon(color=cluster_colors.get(row['클러스터'], 'medkit'))
             ).add_to(marker_cluster)
         
